f0b4b12a17bf3620c947cc711f45ea3baceb19f8
Charles-Emmanuel <che.camus@gmail.com>
2014-11-16 13:33:32 +0100

    Added new grunt connect for livereload + new example folder

--- Gruntfile.js
+++ Gruntfile.js
@@ -7,6 +7,12 @@ module.exports = function(grunt) {
   require('time-grunt')(grunt);
 
   grunt.initConfig({
+    wemucs: {
+      dirs: {
+        examples: './examples'
+      }
+    },
+
     pkg: grunt.file.readJSON('package.json'),
 
     bower: {
@@ -38,11 +44,36 @@ module.exports = function(grunt) {
       sass: {
         files: 'scss/**/*.scss',
         tasks: ['sass']
+      },
+      livereload: {
+        options: {
+          livereload: '<%= connect.options.livereload %>'
+        },
+        files: [
+          '<%= wemucs.dirs.examples %>/index.html'
+        ]
       }
-    }
+    },
+
+    connect: {
+      options: {
+        port: 9000,
+        livereload: 35729,
+        // Change this to '0.0.0.0' to access the server from outside
+        hostname: '0.0.0.0'
+      },
+      livereload: {
+        options: {
+          open: true,
+          base: [
+            '<%= wemucs.dirs.examples %>'
+          ]
+        }
+      }
+    },
   });
 
   grunt.registerTask('build', ['sass']);
   grunt.registerTask('default', ['bower', 'build']);
-  grunt.registerTask('serve', ['default', 'watch']);
+  grunt.registerTask('serve', ['default', 'connect:livereload', 'watch']);
 };
